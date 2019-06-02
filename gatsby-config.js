da645455a46e9119ac02fcfaf7a961ddf1674f55
Yichaoz <lis.yichao@gmail.com>
2019-06-02 11:13:17 -0500

    fix post index

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,21 +10,21 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/content/pages`,
+        path: `${__dirname}/content/pages/`,
         name: 'pages',
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/blog`,
+        path: `${__dirname}/content/blog/`,
         name: `blog`,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/assets`,
+        path: `${__dirname}/content/assets/`,
         name: `assets`,
       },
     },
